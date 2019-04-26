f2777f0735eabf2acdb08be08566ec8027bd3d54
SaraQian <abigail830@163.com>
2019-04-26 19:04:43 +0800

    init commit

---
+++ src/main/java/com/github/abigail830/mybatictest/MybaticTestApplication.java
@@ -0,0 +1,13 @@
+package com.github.abigail830.mybatictest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MybaticTestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MybaticTestApplication.class, args);
+	}
+
+}
